5a3771ff6a0d73534de3b2355e0f3e790fe50ba8
Christopher Kaczenski <kzpulaski@gmail.com>
2016-02-16 22:14:03 -0600

    Parse New User created for signup

--- app/src/main/java/com/teamtreehouse/ribbit/RibbetApplication.java
+++ app/src/main/java/com/teamtreehouse/ribbit/RibbetApplication.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.ribbit;
 
+
 import android.app.Application;
 
 import com.parse.Parse;
@@ -12,13 +13,10 @@ public class RibbetApplication extends Application {
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.parse_application_id))
-                .clientKey(getString(R.string.parse_application_id))
+                .clientKey(getString(R.string.parse_client_secret))
                 .server(getString(R.string.server_address))
                 .build()
         );
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 }
