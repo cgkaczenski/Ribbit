f76418e02f736ee4fda35a3e3979e4e332968a61
Christopher Kaczenski <kzpulaski@gmail.com>
2016-02-17 12:51:12 -0600

    Modifying tabs and fragments from the template.

---
+++ app/src/main/java/com/teamtreehouse/ribbit/SectionsPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.teamtreehouse.ribbit;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        switch(position) {
+            case 0:
+                return new InboxFragment();
+            case 1:
+                return new FriendsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1);
+            case 1:
+                return mContext.getString(R.string.title_section2);
+        }
+        return null;
+    }
+}
