0b159e82699ac9115c466aba068a1a8f898c7b0c
Christopher Kaczenski <kzpulaski@gmail.com>
2016-02-19 16:23:01 -0600

    Camera functionality added: Camera toolbar button, Photo intent, photo storage.

--- app/src/main/java/com/teamtreehouse/ribbit/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/MainActivity.java
@@ -1,9 +1,18 @@
 package com.teamtreehouse.ribbit;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -15,13 +24,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    protected ProgressBar mProgressBar;
+
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+    public static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 6;
+
+    protected Uri mMediaUri;
+
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -32,11 +58,152 @@ public class MainActivity extends AppCompatActivity {
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
+     //The {@link ViewPager} that will host the section contents.
     private ViewPager mViewPager;
-    protected ProgressBar mProgressBar;
+
+    //OnClickListener for ToolBar Camera button
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            isWriteExternalPermissionGranted();
+            switch (which) {
+                case 0:
+                    takePicture();
+                    break;
+                case 1: //take video
+                    break;
+                case 2: //choose picture
+                    break;
+                case 3: //choose video
+                    break;
+            }
+        }
+
+        private void takePicture() {
+            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+            if (mMediaUri == null){
+                Toast.makeText(MainActivity.this,
+                        R.string.error_external_storage,
+                        Toast.LENGTH_LONG).show();
+            }else {
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+            }
+        }
+
+        private Uri getOutputMediaFileUri(int mediaType) {
+            // To be safe, check that Memory Storage(SD Card or built-in memory) is mounted
+            // using Environment.getExternalStorageState() before doing this.
+            if(isExternalStorageAvailable()){// Get URI
+
+                String appName = MainActivity.this.getString(R.string.app_name);
+                // 1.get external storage directory
+                File mediaStorageDir = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                        appName);
+
+                // 2.create subdirectory
+                if (! mediaStorageDir.exists()) {
+                    if (! mediaStorageDir.mkdir()) {
+                        Log.e(TAG, "Failed to create directory");
+                        return null;
+                    }
+                }
+
+                // 3.create a filename and file
+                File mediaFile;
+                Date now = new Date();
+                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+                String path = mediaStorageDir.getPath() + File.separator;
+                if (mediaType == MEDIA_TYPE_IMAGE) {
+                    mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+                }else if (mediaType == MEDIA_TYPE_VIDEO){
+                    mediaFile = new File(path + "VID_" + timestamp + ".mp4");
+                }else {
+                    return null;
+                }
+
+                //4. return file's URL
+                Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+                return Uri.fromFile(mediaFile);
+            }else{
+                return null;
+            }
+        }
+
+        private boolean isExternalStorageAvailable() {
+            String state = Environment.getExternalStorageState();
+            return state.equals(Environment.MEDIA_MOUNTED);
+        }
+    };
+
+
+    private boolean isWriteExternalPermissionGranted() {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // We do not have permission to write
+
+                // Should we show an explanation? This method only returns true if the user has previously denied a request.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need the permission, then prompt for it.
+                    // You can do this how you want, I just like snackbars :)
+                    showWriteToStorageSnackbar();
+                } else {
+                    requestWritePermissionWithCallback();
+
+                    // PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+
+                // We don't have permission right now, but the user has been prompted.
+                return false;
+            }
+            return true;
+        }
+
+        private void showWriteToStorageSnackbar() {
+            Snackbar.make(mViewPager, "Write to storage is required to store and access photos/videos.",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            requestWritePermissionWithCallback();
+                        }
+                    }).show();
+        }
+
+        private void requestWritePermissionWithCallback() {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission was granted, yay!
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    showWriteToStorageSnackbar();
+                }
+                break;
+            default:
+                Log.e(TAG, "Got request code: " + requestCode + " which is not used in switch.");
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +239,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
     }
 
     private void navigateToLogin() {
@@ -96,13 +262,18 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            ParseUser.logOut();
-            navigateToLogin();
-        } else if(id == R.id.action_edit_friends){
-            Intent intent = new Intent(this, EditFriendsActivity.class);
-            startActivity(intent);
+        switch (id) {
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
+            case R.id.action_edit_friends:
+                Intent intent = new Intent(this, EditFriendsActivity.class);
+                startActivity(intent);
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
         }
 
         return super.onOptionsItemSelected(item);
